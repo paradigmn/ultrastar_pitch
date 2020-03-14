6a776f41104a2f865ca77f737688bbcf8aacf0f9
paradigm <ultrastarpitch@gmail.com>
2020-03-14 20:23:56 +0100

    implemented markov transition matrices

--- setup.spec
+++ setup.spec
@@ -8,6 +8,7 @@ FFMPEG_BIN = 'ffmpeg.exe'
 FFMPEG_DIR = 'ffmpeg\\bin\\'
 MODEL_NAME = 'tf2_256_96_12_stft_pca_median.model'
 MODEL_PATH = 'ultrastar_pitch\\binaries\\tf2_256_96_12_stft_pca_median.model\\'
+MARKOV_TRANS = 'makov_trans_cube.npy'
 PCA_COMP = 'pca_components.npy'
 PCA_MEAN = 'pca_mean.npy'
 BIN_DIR = 'ultrastar_pitch\\binaries\\'
@@ -31,6 +32,7 @@ pyz = PYZ(a.pure, a.zipped_data, cipher=block_cipher)
 exe = EXE(pyz,
           a.scripts,
           a.binaries + [(FFMPEG_BIN, os.path.join(FFMPEG_DIR, FFMPEG_BIN), 'BINARY')] +
+                       [(MARKOV_TRANS, os.path.join(BIN_DIR, MARKOV_TRANS), 'BINARY')] +
                        [(PCA_COMP, os.path.join(BIN_DIR, PCA_COMP), 'BINARY')] +
                        [(PCA_MEAN, os.path.join(BIN_DIR, PCA_MEAN), 'BINARY')],
           a.zipfiles,
--- ultrastar_pitch/postprocessing.py
+++ ultrastar_pitch/postprocessing.py
@@ -7,10 +7,12 @@
 @author        paradigm
 """
 
+import os
+import sys
 import numpy as np
 
 class Markov:
-    """ """
+    """ imporoove the pitch detection by applying statistical postprocessing with markov chains """
     # circle of five as matrix representation
     # the rows represent the dur key, while the columns correspond to the pitches
     key_table = np.matrix([[1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 0, 1],
@@ -25,70 +27,29 @@ class Markov:
                            [0, 1, 1, 0, 1, 0, 1, 0, 1, 1, 0, 1],
                            [1, 0, 1, 1, 0, 1, 0, 1, 0, 1, 1, 0],
                            [0, 1, 0, 1, 1, 0, 1, 0, 1, 0, 1, 1]])
-    # transformation matrices to describe the makov chains for each key
-    trans_mat=np.array([
-                np.matrix([[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                           [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                           [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                           [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                           [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                           [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                           [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                           [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                           [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                           [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                           [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                           [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]),
 
-                np.matrix([[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                           [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                           [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                           [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                           [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                           [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                           [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                           [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                           [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                           [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                           [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                           [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]),
+    def __init__(self, trans_cube=None):
+        """ load class variables\n
+        @param   trans_cube   12x12x12 numpy array containing the transition matrices for each key
+        """
+        if trans_cube:
+            # load transition matrices from external source
+            self.trans_cube = trans_cube
+        elif getattr(sys, 'frozen', False):
+            # load transition matrices from meipass in case of binary execution
+            self.trans_cube = np.load(os.path.join(sys._MEIPASS, "makov_trans_cube.npy"))
+        else:
+            # load transition matrices from binary folder
+            self.trans_cube = np.load(os.path.join(
+                os.path.dirname(__file__), "binaries", "makov_trans_cube.npy"))
 
-                np.matrix([[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                           [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                           [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                           [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                           [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                           [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                           [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                           [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                           [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                           [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                           [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                           [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]),
-
-                np.matrix([[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                           [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                           [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                           [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                           [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                           [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                           [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                           [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                           [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                           [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                           [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                           [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]])])
-    
-    def __init__(self):
-        pass
-    
     def detect_key(self, pitches):
-        """ find the key of a song by the detected pitches\n
-        @param  pitches  list of detected pitches\n
-        @return the key of the song (0-11)
+        """ find the key of a song by analysing the detected pitches\n
+        @param   pitches   list of detected pitches\n
+        @return  the key of the song (0-11)
         """
         # count the occurance of every pitch
-        pitch_distribution = [np.count_nonzero(np.aarray(pitches) == x) for x in range(12)]
+        pitch_distribution = [np.count_nonzero(np.array(pitches) == x) for x in range(12)]
         # calculate the weight for each key to dertermine the most likely one
         key_weights = self.key_table @ pitch_distribution
         return key_weights.argmax()
